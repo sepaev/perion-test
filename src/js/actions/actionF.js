18cd41aafcd6de976078e0dc5b26e0f8bce5ed1d
sepaev <sepaev@gmail.com>
2022-01-05 00:38:21 +0200

    Action E

--- src/js/actions/actionF.js
+++ src/js/actions/actionF.js
@@ -1,3 +1,13 @@
-export function actionF() {
-  console.log('F');
+export function actionF([fansLeft, fansRight], tagLines, [leftTeam, rightTeam]) {
+  tagLines.forEach(text => {
+    text.style.opacity = 0;
+    text.style.pointerEvents = 'none';
+  });
+  leftTeam.style.transform = 'translateX(-200%)';
+  fansLeft.style.transform = 'translate(-100%, -140%)';
+  rightTeam.style.transform = 'translateX(200%)';
+  fansRight.style.transform = 'translate(100%, 130%)';
+
+  leftTeam.style.animation = 'teamsHideLeftAnimation 800ms 1 linear';
+  rightTeam.style.animation = 'teamsHideRightAnimation 800ms 1 linear';
 }
--- src/js/modal.js
+++ src/js/modal.js
@@ -3,8 +3,7 @@ import { actionA, actionB, actionC, actionD, actionE } from './actions';
 import { actionF, actionG, actionH, actionI, actionJ } from './actions';
 import { actionK, actionL, actionM, actionN, actionO, actionP } from './actions';
 const actionTime = 250;
-const startDelay = actionTime * 4;
-
+const startDelay = actionTime * 0;
 const { confettiBox, cupHoist, body, modalWindow, closeButton } = getRefs();
 const { teams, fansGate, tagLines } = getRefs();
 
@@ -19,11 +18,18 @@ function openModal() {
   modalWindow.classList.toggle('isOpen');
   body.classList.toggle('modalIsOpen');
   closeButton.addEventListener('click', closeModalWindow);
-  window.setTimeout(() => actionA(cupHoist, confettiBox), actionTime * 0); // 0 + 500 => 500
-  window.setTimeout(() => actionB(), actionTime * 1); // 250 + 2000 => 2250
-  window.setTimeout(() => actionC(fansGate), actionTime * 3); //750 + 1500 => 2250
-  window.setTimeout(() => actionD(tagLines), actionTime * 7); // 1750 + 1800 => 3550
-  window.setTimeout(() => actionE(teams), actionTime * 15); // 3750 + 500
+  let delayAction = 0; //0
+  window.setTimeout(() => actionA(cupHoist, confettiBox), delayAction); // 0 + 500 => 500
+  delayAction += actionTime; //250
+  window.setTimeout(() => actionB(), delayAction); // 250 + 2000 => 2250
+  delayAction += actionTime * 2; //750
+  window.setTimeout(() => actionC(fansGate), delayAction); //750 + 1500 => 2250
+  delayAction += actionTime * 4; //1750
+  window.setTimeout(() => actionD(tagLines), delayAction); // 1750 + 1800 => 3550
+  delayAction += actionTime * 8; //3750
+  window.setTimeout(() => actionE(teams), delayAction); // 3750 + 1000 => 4750
+  delayAction += actionTime * 8; //4750
+  window.setTimeout(() => actionF(fansGate, tagLines, teams), delayAction); // 5750 + ? => 4750
   actionF();
   actionG();
   actionH();
